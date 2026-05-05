02f6ad3ac9bebce8d2e43bbeb2a3b026d49176f9
Joanne Hoar <joanne.hoar@gmail.com>
2026-05-05 13:24:01 -0600

    generated header component and added to app root

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/header/header';
+
+const pi = 3.14;
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected readonly title = signal('Joanne\'s Everyday Market');
+  protected readonly title = signal('Joanne\'s Everyday Market ' + pi);
 }
