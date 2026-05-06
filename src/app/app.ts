40bfaa26a75a888e5f36f9ddee2aa0ddbd2868f3
Joanne Hoar <joanne.hoar@gmail.com>
2026-05-06 15:22:09 -0600

    added product card component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
+import { Product } from './market/product';
+import { ProductCard } from './market/product-card/product-card';
 
 const pi = 3.14;
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, ProductCard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 
 export class App {
   protected readonly title = signal('Joanne\'s Everyday Market ' + pi);
+
+  // used ChatGPT 5.4 with sytnax for instantiation
+  product:Product = {
+    id: 1,
+    name: "Test Product",
+    price: 9.99,
+    description: "This is a test product",
+    image: "test.png"
+  };
 }
