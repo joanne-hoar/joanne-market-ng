8990a98bf16e76ea490dfd99b17b334f53275695
Joanne Hoar <joanne.hoar@gmail.com>
2026-05-10 08:30:28 -0600

    Moved Product Service and Product Card into Product Page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,16 @@
-import { Component, signal, inject } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
-import { ProductService } from './market/product-service';
-import { ProductCard } from './market/product-card/product-card';
 
 const pi = 3.14;
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, ProductCard],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 
 export class App {
   protected readonly title = signal('Joanne\'s Everyday Market ' + pi);
-
-  productService = inject(ProductService);
 }
--- src/app/product-page/product-page.ts
+++ src/app/product-page/product-page.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductService } from '../market/product-service';
+import { ProductCard } from '../market/product-card/product-card';
 
 @Component({
   selector: 'app-product-page',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './product-page.html',
   styleUrl: './product-page.css',
 })
-export class ProductPage {}
+export class ProductPage {
+    productService = inject(ProductService);
+}
