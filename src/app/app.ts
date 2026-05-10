d54f33369fe970984163f023cec07edfb975a648
Joanne Hoar <joanne.hoar@gmail.com>
2026-05-10 09:06:52 -0600

    Customized header with image and css styles; added favicon.ico and pass in the app title

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,8 +2,6 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
 
-const pi = 3.14;
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Header],
@@ -12,5 +10,5 @@ const pi = 3.14;
 })
 
 export class App {
-  protected readonly title = signal('Joanne\'s Everyday Market ' + pi);
+  protected readonly title = signal('Joanne\'s Pet Shop Market');
 }
--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  readonly title = input.required<string>();
+}
